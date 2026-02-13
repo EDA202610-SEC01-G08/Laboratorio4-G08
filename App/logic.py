257da4af2cb5c6519feef0dcce05acd1cd84b5fe
zeta976 <a.zuluagar@uniandes.edu.co>
2026-02-13 07:49:26 -0500

    Fix stack

--- App/logic.py
+++ App/logic.py
@@ -29,6 +29,7 @@ import os
 import time
 from DataStructures.List import array_list as lt
 from DataStructures import queue as q
+from DataStructures import stack as st
 # TODO Importar las librerías correspondientes para el manejo de pilas y colas
 
 data_dir = os.path.dirname(os.path.realpath('__file__')) + '/Data/'
--- DataStructures/Stack/stack.py
+++ DataStructures/Stack/stack.py
@@ -1,29 +1,30 @@
 def new_stack():    return {"size": 0, "first": None, "last": None}
 
 def push(stack, item):
+    new_node = {'info': item, 'next': None}
     stack["size"] += 1
     if stack["first"] is None:
-        stack["first"] = item
-        stack["last"] = item
+        stack["first"] = new_node
+        stack["last"] = new_node
     else:
-        stack["last"] = item
+        new_node['next'] = stack["last"]
+        stack["last"] = new_node
 
 def pop(stack):   
-    if stack["size"] == 0:
-        raise IndexError("pop from empty stack")
-    item = stack["last"]
-    stack["size"] -= 1
-    if stack["size"] == 0:
-        stack["first"] = None
-        stack["last"] = None
+#Elimina y retorna el elemento en el tope de la pila my_stack no vacía.
+    if stack["size"] > 0:
+        top_node = stack["last"]
+        stack["size"] -= 1
+        if stack["size"] == 0:
+            stack["first"] = None
+            stack["last"] = None
+        else:
+            stack["last"] = top_node['next']
+        return top_node['info']
     else:
-        # In a real implementation, we would need to update the last element properly.
-        # For now, we assume that popping is only done in a controlled way.
-        pass
-    return item
-
+        raise Exception('EmptyStructureError: stack is empty')
 def is_empty(stack):   return stack["size"] == 0
 
-def top(stack):   return stack["last"] if stack["size"] > 0 else None   
+def top(stack):   return stack["last"]['info'] if stack["size"] > 0 else None   
 
 def size(stack):  return stack["size"]
